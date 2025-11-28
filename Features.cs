1f9f8f7b9842d9f6acfe6c6eb88d8e113d26fed7
yvzn <mail@ludeo.net>
2025-11-28 16:36:02 +0100

    Configure default CSS theme

--- Features.cs
+++ Features.cs
@@ -3,6 +3,7 @@ namespace library_management_system;
 public class Features
 {
 	public int DefaultLoanDuration { get; set; } = 28;
+	public string DefaultBootstrapTheme { get; set; } = "dark";
 	public bool LaunchBrowserOnStartup { get; set; } = false;
 	public bool OnlineBookSearch { get; set; } = true;
 	public bool OnlineMovieSearch { get; set; } = true;
